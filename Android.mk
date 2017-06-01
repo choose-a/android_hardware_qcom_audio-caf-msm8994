omnirom/android_hardware_qcom_audio-caf-msm8992

Only use if TARGET_QCOM_DISPLAY_VARIANT is set in BoardConfig

Android.mk
@@ -1,3 +1,4 @@
+ifeq ($(TARGET_QCOM_AUDIO_VARIANT),caf-msm8992)
 ifneq ($(filter mpq8092 msm8960 msm8226 msm8x26 msm8610 msm8974 msm8x74 apq8084 msm8916 msm8994 msm8992 msm8909,$(TARGET_BOARD_PLATFORM)),)
 
 MY_LOCAL_PATH := $(call my-dir)
@@ -18,4 +19,5 @@ include $(MY_LOCAL_PATH)/audiod/Android.mk
 include $(MY_LOCAL_PATH)/post_proc/Android.mk
 endif
 
+endif
 endif

choose-a/android_hardware_qcom_audio-caf-msm8994

Change-Id: I78c470b08231cf56cee2ece1d39f213d2254c41c

Android.mk
@@ -1,3 +1,4 @@
+ifeq ($(TARGET_QCOM_AUDIO_VARIANT),caf-msm8994)
 ifneq ($(filter mpq8092 msm8960 msm8226 msm8x26 msm8610 msm8974 msm8x74 apq8084 msm8916 msm8994 msm8992 msm8909,$(TARGET_BOARD_PLATFORM)),)
 
 MY_LOCAL_PATH := $(call my-dir)
@@ -18,4 +19,5 @@ include $(MY_LOCAL_PATH)/audiod/Android.mk
 include $(MY_LOCAL_PATH)/post_proc/Android.mk
 endif
 
+endif
 endif