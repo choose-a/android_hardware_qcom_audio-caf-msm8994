omnirom/android_hardware_qcom_audio-caf-msm8992

configs: adjust path for our HAL split
Change-Id: Icdf347ea5d93583e1f69108270bd79640ea430c6

configs/msm8992/msm8992.mk
@@ -47,7 +47,7 @@ AUDIO_FEATURE_ENABLED_DS2_DOLBY_DAP := true
 
 
 #Audio Specific device overlays
-DEVICE_PACKAGE_OVERLAYS += hardware/qcom/audio/configs/common/overlay
+DEVICE_PACKAGE_OVERLAYS += hardware/qcom/audio-caf-msm8992/configs/common/overlay
 
 USE_XML_AUDIO_POLICY_CONF := 1
 
@@ -57,28 +57,28 @@ PRODUCT_COPY_FILES += \
     device/qcom/common/media/audio_policy.conf:system/etc/audio_policy.conf
 else
 PRODUCT_COPY_FILES += \
-    hardware/qcom/audio/configs/msm8992/audio_policy.conf:system/etc/audio_policy.conf
+    hardware/qcom/audio-caf-msm8992/configs/msm8992/audio_policy.conf:system/etc/audio_policy.conf
 endif
 
 PRODUCT_COPY_FILES += \
-    hardware/qcom/audio/configs/msm8992/audio_output_policy.conf:system/vendor/etc/audio_output_policy.conf \
-    hardware/qcom/audio/configs/msm8992/audio_effects.conf:system/vendor/etc/audio_effects.conf \
-    hardware/qcom/audio/configs/msm8992/mixer_paths.xml:system/etc/mixer_paths.xml \
-    hardware/qcom/audio/configs/msm8992/mixer_paths_i2s.xml:system/etc/mixer_paths_i2s.xml \
-    hardware/qcom/audio/configs/msm8992/audio_platform_info.xml:system/etc/audio_platform_info.xml \
-    hardware/qcom/audio/configs/msm8992/audio_platform_info_i2s.xml:system/etc/audio_platform_info_i2s.xml \
-    hardware/qcom/audio/configs/msm8992/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
-    hardware/qcom/audio/configs/msm8992/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml \
-    hardware/qcom/audio/configs/msm8992/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt
+    hardware/qcom/audio-caf-msm8992/configs/msm8992/audio_output_policy.conf:system/vendor/etc/audio_output_policy.conf \
+    hardware/qcom/audio-caf-msm8992/configs/msm8992/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    hardware/qcom/audio-caf-msm8992/configs/msm8992/mixer_paths.xml:system/etc/mixer_paths.xml \
+    hardware/qcom/audio-caf-msm8992/configs/msm8992/mixer_paths_i2s.xml:system/etc/mixer_paths_i2s.xml \
+    hardware/qcom/audio-caf-msm8992/configs/msm8992/audio_platform_info.xml:system/etc/audio_platform_info.xml \
+    hardware/qcom/audio-caf-msm8992/configs/msm8992/audio_platform_info_i2s.xml:system/etc/audio_platform_info_i2s.xml \
+    hardware/qcom/audio-caf-msm8992/configs/msm8992/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
+    hardware/qcom/audio-caf-msm8992/configs/msm8992/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml \
+    hardware/qcom/audio-caf-msm8992/configs/msm8992/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt
 
 #XML Audio configuration files
 ifeq ($(USE_XML_AUDIO_POLICY_CONF), 1)
 ifeq ($(TARGET_USES_AOSP), true)
 PRODUCT_COPY_FILES += \
-    $(TOPDIR)hardware/qcom/audio/configs/common/audio_policy_configuration.xml:/system/etc/audio_policy_configuration.xml
+    $(TOPDIR)hardware/qcom/audio-caf-msm8992/configs/common/audio_policy_configuration.xml:/system/etc/audio_policy_configuration.xml
 else
 PRODUCT_COPY_FILES += \
-    $(TOPDIR)hardware/qcom/audio/configs/msm8992/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml
+    $(TOPDIR)hardware/qcom/audio-caf-msm8992/configs/msm8992/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml
 endif
 PRODUCT_COPY_FILES += \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:/system/etc/a2dp_audio_policy_configuration.xml \
@@ -90,7 +90,7 @@ endif
 
 # Listen configuration file
 PRODUCT_COPY_FILES += \
-    $(TOPDIR)hardware/qcom/audio/configs/msm8992/listen_platform_info.xml:system/etc/listen_platform_info.xml
+    $(TOPDIR)hardware/qcom/audio-caf-msm8992/configs/msm8992/listen_platform_info.xml:system/etc/listen_platform_info.xml
 
 PRODUCT_PACKAGES += \
     libqcomvisualizer \

configs/msm8994/msm8994.mk
@@ -48,7 +48,7 @@ AUDIO_FEATURE_ENABLED_DS2_DOLBY_DAP := true
 
 
 #Audio Specific device overlays
-DEVICE_PACKAGE_OVERLAYS += hardware/qcom/audio/configs/common/overlay
+DEVICE_PACKAGE_OVERLAYS += hardware/qcom/audio-caf-msm8992/configs/common/overlay
 
 USE_XML_AUDIO_POLICY_CONF := 1
 
@@ -58,28 +58,28 @@ PRODUCT_COPY_FILES += \
     device/qcom/common/media/audio_policy.conf:system/etc/audio_policy.conf
 else
 PRODUCT_COPY_FILES += \
-    hardware/qcom/audio/configs/msm8994/audio_policy.conf:system/etc/audio_policy.conf
+    hardware/qcom/audio-caf-msm8992/configs/msm8994/audio_policy.conf:system/etc/audio_policy.conf
 endif
 
 PRODUCT_COPY_FILES += \
-    hardware/qcom/audio/configs/msm8994/audio_output_policy.conf:system/vendor/etc/audio_output_policy.conf \
-    hardware/qcom/audio/configs/msm8994/audio_effects.conf:system/vendor/etc/audio_effects.conf \
-    hardware/qcom/audio/configs/msm8994/mixer_paths.xml:system/etc/mixer_paths.xml \
-    hardware/qcom/audio/configs/msm8994/mixer_paths_i2s.xml:system/etc/mixer_paths_i2s.xml \
-    hardware/qcom/audio/configs/msm8994/audio_platform_info.xml:system/etc/audio_platform_info.xml \
-    hardware/qcom/audio/configs/msm8994/audio_platform_info_i2s.xml:system/etc/audio_platform_info_i2s.xml \
-    hardware/qcom/audio/configs/msm8994/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
-    hardware/qcom/audio/configs/msm8994/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml \
-    hardware/qcom/audio/configs/msm8994/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt
+    hardware/qcom/audio-caf-msm8992/configs/msm8994/audio_output_policy.conf:system/vendor/etc/audio_output_policy.conf \
+    hardware/qcom/audio-caf-msm8992/configs/msm8994/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    hardware/qcom/audio-caf-msm8992/configs/msm8994/mixer_paths.xml:system/etc/mixer_paths.xml \
+    hardware/qcom/audio-caf-msm8992/configs/msm8994/mixer_paths_i2s.xml:system/etc/mixer_paths_i2s.xml \
+    hardware/qcom/audio-caf-msm8992/configs/msm8994/audio_platform_info.xml:system/etc/audio_platform_info.xml \
+    hardware/qcom/audio-caf-msm8992/configs/msm8994/audio_platform_info_i2s.xml:system/etc/audio_platform_info_i2s.xml \
+    hardware/qcom/audio-caf-msm8992/configs/msm8994/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
+    hardware/qcom/audio-caf-msm8992/configs/msm8994/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml \
+    hardware/qcom/audio-caf-msm8992/configs/msm8994/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt
 
 #XML Audio configuration files
 ifeq ($(USE_XML_AUDIO_POLICY_CONF), 1)
 ifeq ($(TARGET_USES_AOSP), true)
 PRODUCT_COPY_FILES += \
-    $(TOPDIR)hardware/qcom/audio/configs/common/audio_policy_configuration.xml:/system/etc/audio_policy_configuration.xml
+    $(TOPDIR)hardware/qcom/audio-caf-msm8992/configs/common/audio_policy_configuration.xml:/system/etc/audio_policy_configuration.xml
 else
 PRODUCT_COPY_FILES += \
-    $(TOPDIR)hardware/qcom/audio/configs/msm8994/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml
+    $(TOPDIR)hardware/qcom/audio-caf-msm8992/configs/msm8994/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml
 endif
 PRODUCT_COPY_FILES += \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:/system/etc/a2dp_audio_policy_configuration.xml \
@@ -91,7 +91,7 @@ endif
 
 # Listen configuration file
 PRODUCT_COPY_FILES += \
-    $(TOPDIR)hardware/qcom/audio/configs/msm8994/listen_platform_info.xml:system/etc/listen_platform_info.xml
+    $(TOPDIR)hardware/qcom/audio-caf-msm8992/configs/msm8994/listen_platform_info.xml:system/etc/listen_platform_info.xml
 
 PRODUCT_PACKAGES += \
     libqcomvisualizer \

choose-a/android_hardware_qcom_audio-caf-msm8994

configs/msm8992/msm8992.mk
@@ -47,7 +47,7 @@ AUDIO_FEATURE_ENABLED_DS2_DOLBY_DAP := true
 
 
 #Audio Specific device overlays
-DEVICE_PACKAGE_OVERLAYS += hardware/qcom/audio/configs/common/overlay
+DEVICE_PACKAGE_OVERLAYS += hardware/qcom/audio-caf-msm8994/configs/common/overlay
 
 USE_XML_AUDIO_POLICY_CONF := 1
 
@@ -57,28 +57,28 @@ PRODUCT_COPY_FILES += \
     device/qcom/common/media/audio_policy.conf:system/etc/audio_policy.conf
 else
 PRODUCT_COPY_FILES += \
-    hardware/qcom/audio/configs/msm8992/audio_policy.conf:system/etc/audio_policy.conf
+    hardware/qcom/audio-caf-msm8994/configs/msm8992/audio_policy.conf:system/etc/audio_policy.conf
 endif
 
 PRODUCT_COPY_FILES += \
-    hardware/qcom/audio/configs/msm8992/audio_output_policy.conf:system/vendor/etc/audio_output_policy.conf \
-    hardware/qcom/audio/configs/msm8992/audio_effects.conf:system/vendor/etc/audio_effects.conf \
-    hardware/qcom/audio/configs/msm8992/mixer_paths.xml:system/etc/mixer_paths.xml \
-    hardware/qcom/audio/configs/msm8992/mixer_paths_i2s.xml:system/etc/mixer_paths_i2s.xml \
-    hardware/qcom/audio/configs/msm8992/audio_platform_info.xml:system/etc/audio_platform_info.xml \
-    hardware/qcom/audio/configs/msm8992/audio_platform_info_i2s.xml:system/etc/audio_platform_info_i2s.xml \
-    hardware/qcom/audio/configs/msm8992/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
-    hardware/qcom/audio/configs/msm8992/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml \
-    hardware/qcom/audio/configs/msm8992/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt
+    hardware/qcom/audio-caf-msm8994/configs/msm8992/audio_output_policy.conf:system/vendor/etc/audio_output_policy.conf \
+    hardware/qcom/audio-caf-msm8994/configs/msm8992/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    hardware/qcom/audio-caf-msm8994/configs/msm8992/mixer_paths.xml:system/etc/mixer_paths.xml \
+    hardware/qcom/audio-caf-msm8994/configs/msm8992/mixer_paths_i2s.xml:system/etc/mixer_paths_i2s.xml \
+    hardware/qcom/audio-caf-msm8994/configs/msm8992/audio_platform_info.xml:system/etc/audio_platform_info.xml \
+    hardware/qcom/audio-caf-msm8994/configs/msm8992/audio_platform_info_i2s.xml:system/etc/audio_platform_info_i2s.xml \
+    hardware/qcom/audio-caf-msm8994/configs/msm8992/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
+    hardware/qcom/audio-caf-msm8994/configs/msm8992/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml \
+    hardware/qcom/audio-caf-msm8994/configs/msm8992/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt
 
 #XML Audio configuration files
 ifeq ($(USE_XML_AUDIO_POLICY_CONF), 1)
 ifeq ($(TARGET_USES_AOSP), true)
 PRODUCT_COPY_FILES += \
-    $(TOPDIR)hardware/qcom/audio/configs/common/audio_policy_configuration.xml:/system/etc/audio_policy_configuration.xml
+    $(TOPDIR)hardware/qcom/audio-caf-msm8994/configs/common/audio_policy_configuration.xml:/system/etc/audio_policy_configuration.xml
 else
 PRODUCT_COPY_FILES += \
-    $(TOPDIR)hardware/qcom/audio/configs/msm8992/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml
+    $(TOPDIR)hardware/qcom/audio-caf-msm8994/configs/msm8992/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml
 endif
 PRODUCT_COPY_FILES += \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:/system/etc/a2dp_audio_policy_configuration.xml \
@@ -90,7 +90,7 @@ endif
 
 # Listen configuration file
 PRODUCT_COPY_FILES += \
-    $(TOPDIR)hardware/qcom/audio/configs/msm8992/listen_platform_info.xml:system/etc/listen_platform_info.xml
+    $(TOPDIR)hardware/qcom/audio-caf-msm8994/configs/msm8992/listen_platform_info.xml:system/etc/listen_platform_info.xml
 
 PRODUCT_PACKAGES += \
     libqcomvisualizer \

configs/msm8994/msm8994.mk
@@ -48,7 +48,7 @@ AUDIO_FEATURE_ENABLED_DS2_DOLBY_DAP := true
 
 
 #Audio Specific device overlays
-DEVICE_PACKAGE_OVERLAYS += hardware/qcom/audio/configs/common/overlay
+DEVICE_PACKAGE_OVERLAYS += hardware/qcom/audio-caf-msm8994/configs/common/overlay
 
 USE_XML_AUDIO_POLICY_CONF := 1
 
@@ -58,28 +58,28 @@ PRODUCT_COPY_FILES += \
     device/qcom/common/media/audio_policy.conf:system/etc/audio_policy.conf
 else
 PRODUCT_COPY_FILES += \
-    hardware/qcom/audio/configs/msm8994/audio_policy.conf:system/etc/audio_policy.conf
+    hardware/qcom/audio-caf-msm8994/configs/msm8994/audio_policy.conf:system/etc/audio_policy.conf
 endif
 
 PRODUCT_COPY_FILES += \
-    hardware/qcom/audio/configs/msm8994/audio_output_policy.conf:system/vendor/etc/audio_output_policy.conf \
-    hardware/qcom/audio/configs/msm8994/audio_effects.conf:system/vendor/etc/audio_effects.conf \
-    hardware/qcom/audio/configs/msm8994/mixer_paths.xml:system/etc/mixer_paths.xml \
-    hardware/qcom/audio/configs/msm8994/mixer_paths_i2s.xml:system/etc/mixer_paths_i2s.xml \
-    hardware/qcom/audio/configs/msm8994/audio_platform_info.xml:system/etc/audio_platform_info.xml \
-    hardware/qcom/audio/configs/msm8994/audio_platform_info_i2s.xml:system/etc/audio_platform_info_i2s.xml \
-    hardware/qcom/audio/configs/msm8994/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
-    hardware/qcom/audio/configs/msm8994/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml \
-    hardware/qcom/audio/configs/msm8994/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt
+    hardware/qcom/audio-caf-msm8994/configs/msm8994/audio_output_policy.conf:system/vendor/etc/audio_output_policy.conf \
+    hardware/qcom/audio-caf-msm8994/configs/msm8994/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    hardware/qcom/audio-caf-msm8994/configs/msm8994/mixer_paths.xml:system/etc/mixer_paths.xml \
+    hardware/qcom/audio-caf-msm8994/configs/msm8994/mixer_paths_i2s.xml:system/etc/mixer_paths_i2s.xml \
+    hardware/qcom/audio-caf-msm8994/configs/msm8994/audio_platform_info.xml:system/etc/audio_platform_info.xml \
+    hardware/qcom/audio-caf-msm8994/configs/msm8994/audio_platform_info_i2s.xml:system/etc/audio_platform_info_i2s.xml \
+    hardware/qcom/audio-caf-msm8994/configs/msm8994/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
+    hardware/qcom/audio-caf-msm8994/configs/msm8994/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml \
+    hardware/qcom/audio-caf-msm8994/configs/msm8994/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt
 
 #XML Audio configuration files
 ifeq ($(USE_XML_AUDIO_POLICY_CONF), 1)
 ifeq ($(TARGET_USES_AOSP), true)
 PRODUCT_COPY_FILES += \
-    $(TOPDIR)hardware/qcom/audio/configs/common/audio_policy_configuration.xml:/system/etc/audio_policy_configuration.xml
+    $(TOPDIR)hardware/qcom/audio-caf-msm8994/configs/common/audio_policy_configuration.xml:/system/etc/audio_policy_configuration.xml
 else
 PRODUCT_COPY_FILES += \
-    $(TOPDIR)hardware/qcom/audio/configs/msm8994/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml
+    $(TOPDIR)hardware/qcom/audio-caf-msm8994/configs/msm8994/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml
 endif
 PRODUCT_COPY_FILES += \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:/system/etc/a2dp_audio_policy_configuration.xml \
@@ -91,7 +91,7 @@ endif
 
 # Listen configuration file
 PRODUCT_COPY_FILES += \
-    $(TOPDIR)hardware/qcom/audio/configs/msm8994/listen_platform_info.xml:system/etc/listen_platform_info.xml
+    $(TOPDIR)hardware/qcom/audio-caf-msm8994/configs/msm8994/listen_platform_info.xml:system/etc/listen_platform_info.xml
 
 PRODUCT_PACKAGES += \
     libqcomvisualizer \